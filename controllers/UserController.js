1d04bbcd26f50ac38e93129866c4581d7384d0b0
jaironf <jaironf.94@gmail.com>
2024-05-11 20:25:51 +0200

    Login added

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -13,6 +13,21 @@ const UserController = {
             console.error(error);
         }
     },
+    async login (req, res){
+        try {
+            const user = await User.findOne({
+                email: req.body.email
+            })
+            const token = jwt.sign({_id: user._id}, jwt_secret)
+            if(user.tokens.length > 4) user.tokens.shift();
+            user.tokens.push(token)
+            await user.save();
+            res.send({msg: 'Bienvenid@', token})
+        } catch (error) {
+            console.error(error);
+            res.status(500).send(error)
+        }
+    }
 }
 
 
