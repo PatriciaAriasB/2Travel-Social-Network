6f7c1f3a5c62a91cb10a6ce2ce4cf8166421245f
Patricia 607413324 <patriciapatriciaariasburdeos@gmail.com>
2024-05-13 13:50:52 +0200

    Relationship added in post

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -3,7 +3,10 @@ const Post = require('../models/Post');
 const PostController = {
     async create(req, res){
         try {
-            const post = await Post.create(req.body)
+            const post = await Post.create({
+                ...req.body, 
+                commentId:req.comment._id
+            })
             res.status(201).send(post)
         } catch (error) {
             console.error(error);
